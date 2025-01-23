411b4e7bc787820e7317eb9bc91d553f041eff37
Ajiro <adilismail7654321@gmail.com>
2025-01-23 15:55:19 +0500

    5th commit change images & links

--- src/Containers/Home/index.jsx
+++ src/Containers/Home/index.jsx
@@ -37,20 +37,20 @@ function Home() {
       {
         title: "Massive Fire Breaks Out in Los Angeles",
         description: "A massive fire erupted in downtown LA last night, causing significant damage to properties and prompting evacuations.",
-        image: "./assets/news-110x110-1.jpg",
-        link: "/news-search",
+        image: "https://bloximages.chicago2.vip.townnews.com/oskaloosa.com/content/tncms/assets/v3/editorial/3/72/372708de-269e-59ab-9657-0a1834c0879e/677e0b5e3261c.image.jpg?resize=750%2C500",
+        link: "https://www.aljazeera.com/gallery/2025/1/23/new-rapidly-growing-wildfire-breaks-out-north-of-los-angeles",
       },
       {
         title: "New Virus Outbreak in China Raises Concerns",
         description: "A novel virus has emerged in southern China, sparking health warnings and raising questions about global preparedness.",
-        image: "./assets/news-110x110-2.jpg",
-        link: "/news-search",
+        image: "https://i.ytimg.com/vi/QYPIkMKyEG8/sddefault.jpg",
+        link: "https://theconversation.com/hmpv-may-be-spreading-in-china-heres-what-to-know-about-this-virus-and-why-its-not-cause-for-alarm-246775",
       },
       {
         title: "Breakthrough Technology Unveiled in Silicon Valley",
         description: "Tech giants have revealed cutting-edge AI innovations, set to revolutionize industries worldwide.",
-        image: "./assets/news-110x110-3.jpg",
-        link: "/news-search",
+        image: "https://i.ytimg.com/vi/TY9-l1S_4Pg/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AH-CYAC0AWKAgwIABABGFEgVChlMA8=&rs=AOn4CLDgIV7Ptj_NtKq94WwxTDLCJgm0mA",
+        link: "https://www.iancollmceachern.com/single-post/unveiling-the-untold-stories-the-secret-history-of-silicon-valley",
       },
     ].map((news, index) => (
       <div className={styles.newsCard} key={index}>
@@ -61,7 +61,7 @@ function Home() {
         />
         <h3 className={styles.newsTitle}>{news.title}</h3>
         <p className={styles.newsDescription}>{news.description}</p>
-        <Link to={news.link} className={styles.readMoreLink}>
+        <Link to={news.link} className={styles.readMoreLink} target="_blank" rel="noopener noreferrer">
           Read More
         </Link>
       </div>
